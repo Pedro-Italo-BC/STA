89f2a9d6826961fd13078a854b5bfdf30bc920a0
Pedro Italo <pedroitalobc@gmail.com>
2026-04-27 22:32:20 -0300

    Project Init Draw Triangle

--- game/Main.go
+++ game/Main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	ent "github.com/Pedro-Italo-BC/STA/game/entities"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
-	fmt.Println("Hello World"); 
+	var p *ent.Player = ent.CreatePlayer()
+	ent.PrintStatus(*p)
+
+	rl.SetConfigFlags(rl.FlagWindowResizable)
+	rl.InitWindow(800, 800, "Test")
+
+	defer rl.CloseWindow()
+	var y float32 = 100.0
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.DrawTriangle(
+			rl.Vector2{X: 400, Y: y},
+			rl.Vector2{X: 300, Y: 400},
+			rl.Vector2{X: 500, Y: 400},
+			rl.Red,
+		)
+
+		if rl.IsKeyPressed(rl.KeyW) {
+			y += 20
+		}
+		rl.ClearBackground(rl.Black)
+		rl.DrawText("Bunda", 300, 300, 20, rl.White)
+		rl.EndDrawing()
+	}
 }
--- game/entities/Player.go
+++ game/entities/Player.go
@@ -3,33 +3,34 @@ package entities
 import "fmt"
 
 type Player struct {
-	pos_x int;
-	pos_y int;
-	health int;
-	velocity float64;
-	angle float64;
+	pos_x    int
+	pos_y    int
+	health   int
+	velocity float64
+	angle    float64
 }
 
 func CreatePlayer() *Player {
-	var player Player := {
-		pos_x: 0,
-		pos_y: 0,
-		health: 3,
+	var player Player = Player{
+		pos_x:    0,
+		pos_y:    0,
+		health:   3,
 		velocity: 1.0,
-		angle: 0.0
+		angle:    0.0,
 	}
 
-	return &player;
+	return &player
 }
 
 func PrintStatus(player Player) {
-	var message string = fmt.Stringf("
-	X: %d\n
-	Y: %d\n
-	health: %d\n
-	velocity: %f\n
-	angle: %f
-	", player.pos_x, player.pos_y, player.health, player.velocity, player.angle);
+	var message string = fmt.Sprintf(
+		"X: %d\nY: %d\nhealth: %d\nvelocity: %f\nangle: %f",
+		player.pos_x,
+		player.pos_y,
+		player.health,
+		player.velocity,
+		player.angle,
+	)
 
-	fmt.Println(message);
+	fmt.Println(message)
 }
